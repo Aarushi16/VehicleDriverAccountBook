adc6965f62ccd0556155cd4c6153832342943d15
Aarushi16 <aarushi.tomar16@gmail.com>
2022-07-05 16:01:35 +0530

    Add project files.

---
+++ VehicleAPI/Context/VehicleDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VehicleAPI.Models;
+
+namespace VehicleAPI.Context
+{
+    public class VehicleDbContext : DbContext
+    {
+        public VehicleDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<Vehicle> Vehicles { get; set; }
+    }
+}
---
+++ VehicleDriverAccountBook/Model/SignInResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthorizationAPI.Model
+{
+    public class SignInResponse
+    {
+        public bool IsSuccess { get; set; }
+        public string Token { get; set; }
+    }
+}
